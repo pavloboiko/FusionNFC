44e23cc0efe3b19fcac0ed32403706e6420d9b9e
Pavlo Boiko <boiko.impul@gmail.com>
2021-06-21 09:04:32 +0300

    no message

--- Sources/FusionNFC_Common/NFCUtility.swift
+++ Sources/FusionNFC_Common/NFCUtility.swift
@@ -1,18 +1,6 @@
 import Foundation
 
-public struct NFCMessage {
-    public let uriRecord: NFCURIRecord?
-    public let textRecord: NFCTextRecord?
-}
-
-public struct NFCURIRecord {
-    public let url: URL
-}
-
-public struct NFCTextRecord {
-    public let string: String
-    public let locale: Locale
-}
+public struct NFCMessage {    public let uriRecord: NFCURIRecord?    public let textRecord: NFCTextRecord?        public init(uriRecord: NFCURIRecord?, textRecord: NFCTextRecord?) {        self.uriRecord = uriRecord        self.textRecord = textRecord    }}public struct NFCURIRecord {    public let url: URL        public init(url: URL) {        self.url = url    }}public struct NFCTextRecord {    public let string: String    public let locale: Locale        public init(string: String, locale: Locale) {        self.string = string        self.locale = locale    }}
 
 public enum SessionUsage {
     case none
