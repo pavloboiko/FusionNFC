82ddb7260f789b4f8e75b802974c4e4fcc070912
Pavlo Boiko <boiko.impul@gmail.com>
2021-06-21 21:13:27 +0300

    no message

--- Sources/FusionNFC_Android/NFCUtility+Android.swift
+++ Sources/FusionNFC_Android/NFCUtility+Android.swift
@@ -7,14 +7,18 @@ import AndroidNFC
 
 import FusionNFC_Common
 
-public class NFCManager {
+public class NFCUtility {
 	private var currentActivity: Activity? { Application.currentActivity }
-	private var adapter: NfcAdapter? = null  
+	private var adapter: NfcAdapter? = nil  
+	private var receiver: Class<NFCReceiver>?
   
 	public required init(alertMessage: String) {
-		let nfcManager = self.currentActivity?.getSystemService(ContextStatic.NFC_SERVICE) as? NfcManager
-        self.adapter = nfcManager.getDefaultAdapter()
+		let nfcManager = self.currentActivity?.getSystemService(name: ContextStatic.NFC_SERVICE) as? NfcManager
+        self.adapter = nfcManager?.getDefaultAdapter()
+        self.receiver = Class<NFCReceiver>()
 	}
+	
+
 }
 
 extension NFCUtility: NFCUtilityProtocol {
@@ -23,11 +27,32 @@ extension NFCUtility: NFCUtilityProtocol {
     }
     
     public func readTag(_ completion: @escaping (NFCMessage?) -> Void) {
-        let intent = self.currentActivity?.getIntent()?.addFlags(IntentStatic.FLAG_ACTIVITY_SINGLE_TOP)
-        let nfcPendingIntent = PendingIntent.getActivity(self.currentActivity, 0, intent, 0)
-        self.adapter?.enableForegroundDispatch(self.currentActivity, nfcPendingIntent, null, null)
+		enableNfcForegroundDispatch()
+		guard let receiver = self.receiver else {
+			completion(nil)
+			return
+		}
+		receiver.cast(obj: NFCReceiver())?.receiver = completion
     }
     
     public func writeTag(_ message: NFCMessage) {
     }
+    
+    private func enableNfcForegroundDispatch() {
+//        let intent = self.currentActivity?.getIntent()?.addFlags(flags: Intent.FLAG_ACTIVITY_SINGLE_TOP)
+		let intent =  Intent(packageContext: self.currentActivity, cls: self.receiver)
+        let nfcPendingIntent = PendingIntent.getActivity(context: self.currentActivity, requestCode: 0, intent: intent, flags: 0)
+        self.adapter?.enableForegroundDispatch(activity: self.currentActivity, intent: nfcPendingIntent, filters: [], techLists: [])
+    }
+    
+    private func disableNfcForegroundDispatch() {
+		self.adapter?.disableForegroundDispatch(activity: self.currentActivity)
+	}
+}
+
+class NFCReceiver: Object, BroadcastReceiver {
+	var receiver: ((NFCMessage?) -> Void)?
+	public func onReceive(context: Context?, intent: Intent?) {
+		print("hongTae really?")
+    }    
 }
