fdef19a02c14a5f01b9a2d553e272e8bc8abbccf
Pavlo Boiko <boiko.impul@gmail.com>
2021-06-23 11:06:51 +0300

    no message

--- Sources/FusionNFC_Android/NFCUtility+Android.swift
+++ Sources/FusionNFC_Android/NFCUtility+Android.swift
@@ -11,12 +11,12 @@ import FusionNFC_Common
 public class NFCUtility {
 	private var currentActivity: Activity? { Application.currentActivity }
 	private var adapter: NfcAdapter? = nil  
-	private var receiver: Class<NFCReceiver>?
+//	private var receiver: Class<NFCReceiver>?
   
 	public required init(alertMessage: String) {
 		let nfcManager = self.currentActivity?.getSystemService(name: ContextStatic.NFC_SERVICE) as? NfcManager
         self.adapter = nfcManager?.getDefaultAdapter()
-        self.receiver = Class<NFCReceiver>()
+//        self.receiver = Class<NFCReceiver>()
 	}
 	
 
@@ -30,11 +30,11 @@ extension NFCUtility: NFCUtilityProtocol {
     public func readTag(_ completion: @escaping (NFCMessage?) -> Void) {
     	print("pavlo read Tag called")
 		enableNfcForegroundDispatch()
-		guard let receiver = self.receiver else {
-			completion(nil)
-			return
-		}
-		receiver.cast(obj: NFCReceiver())?.receiver = completion
+//		guard let receiver = self.receiver else {
+//			completion(nil)
+//			return
+//		}
+//		receiver.cast(obj: NFCReceiver())?.receiver = completion
     }
     
     public func writeTag(_ message: NFCMessage) {
@@ -43,7 +43,7 @@ extension NFCUtility: NFCUtilityProtocol {
     private func enableNfcForegroundDispatch() {
         print("pavlo start enable")
 //        let intent = self.currentActivity?.getIntent()?.addFlags(flags: Intent.FLAG_ACTIVITY_SINGLE_TOP)
-		let intent =  Intent(packageContext: self.currentActivity, cls: self.receiver)
+		let intent =  Intent(packageContext: self.currentActivity, cls: Class<NFCReceiver>())
         let nfcPendingIntent = PendingIntent.getBroadcast(context: self.currentActivity, requestCode: 0, intent: intent, flags: 0)
         self.adapter?.enableForegroundDispatch(activity: self.currentActivity, intent: nfcPendingIntent, filters: [], techLists: [])
         print("pavlo end enable")        
