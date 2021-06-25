64621c9bfc57bbd49b3946ab7eac3a16c2524714
Pavlo Boiko <boiko.impul@gmail.com>
2021-06-25 23:43:44 +0300

    no message

--- Sources/FusionNFC_Android/Generated/NFCReceiver.java
+++ Sources/FusionNFC_Android/Generated/NFCReceiver.java
@@ -1,9 +1,13 @@
 package FusionNFC_Android;
+import android.util.Log;
 
 public class NFCReceiver extends android.content.BroadcastReceiver {
   private long _ptr;
   
   public void onReceive(android.content.Context context, android.content.Intent intent) {
+  	Log.e("pavlo", context);
+  	Log.e("pavlo", intent);
+  	Log.e("pavlo", _ptr + "");
     onReceiveImpl(_ptr ,context ,intent);
   }
   private native void onReceiveImpl(long _ptr, android.content.Context context, android.content.Intent intent);
