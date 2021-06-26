225a1d27db18e1c009f3bfe639c629005e092e6c
Pavlo Boiko <boiko.impul@gmail.com>
2021-06-27 00:35:25 +0300

    no message

--- Sources/FusionNFC_Android/NFCManager+Android.swift
+++ Sources/FusionNFC_Android/NFCManager+Android.swift
@@ -11,6 +11,7 @@ import FusionNFC_Common
 public class NFCManager {
 	private var currentActivity: Activity? { Application.currentActivity }
 	private var adapter: NfcAdapter? = nil  
+	private var receiver = NFCReceiver()
   
 	public required init(alertMessage: String) {
 		let nfcManager = self.currentActivity?.getSystemService(name: ContextStatic.NFC_SERVICE) as? NfcManager
@@ -36,7 +37,7 @@ extension NFCManager: NFCManagerProtocol {
     private func enableNfcForegroundDispatch() {
         print("pavlo start enable")
 //        let intent = self.currentActivity?.getIntent()?.addFlags(flags: Intent.FLAG_ACTIVITY_SINGLE_TOP)
-		let intent =  Intent(packageContext: self.currentActivity, cls: NFCReceiver.shared.getClass())
+		let intent =  Intent(packageContext: self.currentActivity, cls: receiver.getClass())
 //		print("pavlo receiver get class = \(receiver.getClass())")
         let nfcPendingIntent = PendingIntent.getBroadcast(context: self.currentActivity, requestCode: 0, intent: intent, flags: 0)
         self.adapter?.enableForegroundDispatch(activity: self.currentActivity, intent: nfcPendingIntent, filters: [], techLists: [])
