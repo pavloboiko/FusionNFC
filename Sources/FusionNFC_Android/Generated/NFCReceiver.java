13819176d0b24f5239942d023e76e3e4d9cbcbf7
Pavlo Boiko <boiko.impul@gmail.com>
2021-06-26 10:34:26 +0300

    no message

--- Sources/FusionNFC_Android/Generated/NFCReceiver.java
+++ Sources/FusionNFC_Android/Generated/NFCReceiver.java
@@ -1,13 +1,9 @@
 package FusionNFC_Android;
-import android.util.Log;
 
 public class NFCReceiver extends android.content.BroadcastReceiver {
   private long _ptr;
   
   public void onReceive(android.content.Context context, android.content.Intent intent) {
-  	Log.e("pavlo", context.toString());
-  	Log.e("pavlo", intent.toString());
-  	Log.e("pavlo", _ptr + "");
     onReceiveImpl(_ptr ,context ,intent);
   }
   private native void onReceiveImpl(long _ptr, android.content.Context context, android.content.Intent intent);
--- Sources/FusionNFC_Android/Generated/NFCReceiver.swift
+++ Sources/FusionNFC_Android/Generated/NFCReceiver.swift
@@ -8,14 +8,11 @@ import AndroidContent
 import AndroidNFC
 
 @_silgen_name("Java_FusionNFC_1Android_NFCReceiver_onReceiveImpl")
-public func NFCReceiver_onReceiveImpl(env: UnsafeMutablePointer<JNIEnv>, obj: JavaObject?, ptr: JavaLong, context: JavaObject?, intent: JavaObject?) -> Void {
-//  let _obj = unsafeBitCast(Int(truncatingIfNeeded:ptr), to: NFCReceiver.self)
+public func NFCReceiver_onReceiveImpl(env: UnsafeMutablePointer<JNIEnv>, obj: JavaObject?, ptr: JavaLong, context: JavaObject?, intent: JavaObject?) -> Void { 
+  	let _context = cast(Object?.fromJavaObject(context), to: ContextProxy.self)
+  	let _intent = Intent?.fromJavaObject(intent)
   
-  let _context = cast(Object?.fromJavaObject(context), to: ContextProxy.self)
-  let _intent = Intent?.fromJavaObject(intent)
-  
-//  _obj.onReceive(context: _context, intent: _intent)
-	NFCReceiver.pavlo(context: _context, intent: _intent)
+	NFCReceiver.didReceive(context: _context, intent: _intent)
 }
 
 #endif
